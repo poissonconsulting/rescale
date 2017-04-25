9e500195174c49c2f570958bc40b88a1f1adab17
joethorley <joe@poissonconsulting.ca>
2017-04-24 17:20:51 -0700

    export check_valid_rescalers

--- R/check.R
+++ R/check.R
@@ -3,6 +3,7 @@
 #' @param x A character vector to test for valid rescalers.
 #' @param x_name A string of the object name.
 #' @return An informative error if the check failed otherwise the original object.
+#' @export
 check_valid_rescalers <- function(x, x_name = substitute(x)) {
   if (is.name(x)) x_name %<>% deparse()
   valid <- vapply(x, is_valid_rescaler, TRUE)
