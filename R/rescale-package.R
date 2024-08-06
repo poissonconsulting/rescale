51f633a3d93f52f3b0f114f3d0dd8b9961092ad5
Joe Thorley <joe@poissonconsulting.ca>
2024-08-06 10:27:14 -0700

    usethis::use_package_doc()

--- R/rescale-package.R
+++ R/rescale-package.R
@@ -1,5 +1,10 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
 #' @import chk
 #' @importFrom glue glue
 #' @importFrom magrittr %<>% %>%
 #' @importFrom stats setNames
+## usethis namespace: end
 NULL
