92195ff36a43955a170db3f4084938c7519d25b2
Joe Thorley <joe@poissonconsulting.ca>
2024-08-06 10:29:56 -0700

    testthat3

--- tests/testthat/test-rescale.R
+++ tests/testthat/test-rescale.R
@@ -3,15 +3,15 @@ test_that("rescale works", {
   expect_identical(rescale(mtcars), mtcars)
   expect_identical(rescale(mtcars, center = "mpg")[-1], mtcars[-1])
   expect_identical(rescale(mtcars, scale = "mpg")[-1], mtcars[-1])
-  expect_is(rescale(mtcars, center = "cyl")$cyl, "numeric")
+  expect_type(rescale(mtcars, center = "cyl")$cyl, "double")
   mtcars$cyl <- as.integer(mtcars$cyl)
-  expect_is(rescale(mtcars, center = "cyl")$cyl, "integer")
+  expect_type(rescale(mtcars, center = "cyl")$cyl, "integer")
   mtcars$cyl <- as.numeric(mtcars$cyl)
-  expect_equivalent(
+  expect_equal(
     rescale(mtcars, center = names(mtcars)),
     as.data.frame(base::scale(mtcars, scale = FALSE))
   )
-  expect_equivalent(
+  expect_equal(
     rescale(mtcars, scale = names(mtcars)),
     as.data.frame(base::scale(mtcars))
   )
